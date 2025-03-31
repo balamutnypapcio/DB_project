063c2faabc7e6d914760a55df3ad82703ef6082e
michal_markuzel <markuzel.michal@gmail.com>
2025-03-31 09:37:53 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+https://gist.github.com/bmaupin/0ce79806467804fdbbf8761970511b8c <- free hosting
--- SQL/table_creation.sql
+++ SQL/table_creation.sql
@@ -1,7 +1,5 @@
 USE trickount_DB;
 
-
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
